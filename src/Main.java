34e0e1c59cb54511974ed49da28a0147b8fc89f3
Tural <musaev.tural.94@mail.ru>
2024-06-03 03:16:27 +0300

    домашка до 03 июня

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Gryffindor garry = new Griffinor ("Гарри поттер", 95, 40, 86, 18, 56  );
+        Griffindor germione = new Griffindor ("Гермиона Грейнджер", 80, 45, 90, 30, 35);
+        Griffindor ron = new Griffindor("Рон Визли", 60, 30 , 18, 9, 43 );
+        Hufflepuff zahari = new Hufflepuff("Захария Смит", 35, 60, 86, 35, 16);
+        Hufflepuff sedrik = new Hufflepuff ("Седрик Дигорри",73, 50, 19, 25, 48);
+        Hufflepuff djastin = new Hufflepuff (" Джастин Финч-Флетчли",39, 25, 34, 75, 87 );
+        Ravenslaw  chjo = new Ravenslaw ("Чжоу Чанг", 39, 25, 34, 75, 87, 32);
+        Ravenslaw padma = new Ravenslaw ("Падма Патил", 60, 30 , 18, 9, 43, 32);
+        Ravenslaw markus = new Ravenslaw ("Маркус Белби", 80, 45, 90, 30, 35, 32);
+        Slytherin draco = new Slytherin ("Драко Малфой", 95, 40, 86, 18, 56, 35, 69);
+        Slytherin grehem = new Slytherin("Грехем Монтегю", 35, 65, 86, 35, 16, 19, 65);
+        Slytherin gregori = new Slytherim("Грегори Гойл",39, 25, 34, 75, 87, 42, 17);
+
+        garry.printPowerDiffendes(draco);
+
+
+    }
+}
